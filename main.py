af5c8750218cc06186f9d76a24a0d714c3e6a7db
RohanCalculus <rohan.calculus@gmail.com>
2025-02-16 18:28:49 +0530

    'added

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Crossword Topic For the Day:- Quadratics")
+
+st.write("All the best!")
+
+# Embed the iframe
+st.components.v1.iframe(
+    src="https://crosswordlabs.com/embed/quadratics-163",
+    width=600,
+    height=600
+)
