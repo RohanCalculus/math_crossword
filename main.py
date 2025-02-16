0501daa35f0b8474a523074f9b1d579267a24f90
RohanCalculus <rohan.calculus@gmail.com>
2025-02-16 18:55:54 +0530

    size

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ st.write("Instructions:-")
 st.write('1. You might see some answers right away if someone has not cleared the puzzle.')
 st.write('2. Hence, please click on "Clear Puzzle" button before starting.')
 st.write('3. You are requested to Clear the Puzzle after completing it.')
+st.write('')
+st.write('Please Now Scroll Down to the Puzzle! All the best! <3')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
 # Embed the iframe
 st.components.v1.iframe(
     src="https://crosswordlabs.com/embed/quadratics-163",
